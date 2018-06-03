3444e8b943407fdc3f8417ef5b8b267da579a370
linjs <linjs@woobest.com>
2018-06-03 23:16:31 +0800

    add rope

--- assets/Script/component/Man.js
+++ assets/Script/component/Man.js
@@ -5,7 +5,7 @@ var HookState = cc.Enum({
   FlyBack: 3,
 });
 
-var Radius = 50;
+var Radius = 70;
 var MoveDistance = 8;
 
 var Man = cc.Class({
@@ -13,6 +13,16 @@ var Man = cc.Class({
 
   properties: {
     hook: {
+      default: null,
+      type: cc.Sprite,
+    },
+
+    ropeMask: {
+      default: null,
+      type: cc.Mask,
+    },
+
+    rope: {
       default: null,
       type: cc.Sprite,
     },
@@ -32,10 +42,12 @@ var Man = cc.Class({
     // add key down and key up event
     cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
     this.hook.node.on("CatchGold", this.onCatchGold, this);
+    this.maskHeight = this.ropeMask.node.height;
   },
 
   onDestroy() {
     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    this.hook.node.off("CatchGold", this.onCatchGold, this);
   },
 
   onKeyDown: function (event) {
@@ -77,6 +89,25 @@ var Man = cc.Class({
     }
   },
 
+  setHookRotation: function (angle) {
+    this.hook.node.rotation = angle;
+    this.ropeMask.node.rotation = angle;
+    // this.rope.node.rotation = angle;
+  },
+
+  setHookPos: function (x, y) {
+    let deltaX = x - this.hook.node.x;
+    let deltaY = y - this.hook.node.y;
+    this.hook.node.x = x;
+    this.hook.node.y = y;
+
+    // this.rope.node.x += deltaX;
+    // this.rope.node.y += deltaY;
+    this.ropeMask.node.x = x;
+    this.ropeMask.node.y = y;
+
+  },
+
   rotationHook: function(dt) {
     this.hookAngle += this.angleDelta;
     if (this.hookAngle >= 80)
@@ -87,22 +118,23 @@ var Man = cc.Class({
       this.angleDelta = -this.angleDelta;
       this.hookAngle = -80;
     }
-    this.hook.node.rotation = this.hookAngle;
+    this.setHookRotation(this.hookAngle);
 
     let deltaX = Radius * Math.sin(Math.abs(this.hookAngle) * 0.017453293);
     let deltaY = Radius * Math.cos(Math.abs(this.hookAngle) * 0.017453293);
     let x = this.hookAngle > 0 ? this.hookPosX - deltaX : this.hookPosX + deltaX;
     let y = this.hookPosY + Radius - deltaY;
-    this.hook.node.x = x;
-    this.hook.node.y = y;
+    this.setHookPos(x, y);
     // cc.log(this.hookAngle, Math.sin(this.hookAngle), Math.cos(this.hookAngle), deltaX, deltaY, x, y);
   },
 
   flyOut: function(dt) {
     let deltaX = MoveDistance * Math.sin(Math.abs(this.hookAngle) * 0.017453293);
     let deltaY = MoveDistance * Math.cos(Math.abs(this.hookAngle) * 0.017453293);
-    this.hook.node.x += (this.hookAngle > 0 ?  -deltaX : deltaX);
-    this.hook.node.y -= deltaY;
+    let x = this.hook.node.x + (this.hookAngle > 0 ? -deltaX : deltaX);
+    let y = this.hook.node.y - deltaY;
+    this.setHookPos(x, y);
+    this.ropeMask.node.height += MoveDistance;
 
     let point = this.hook.node.convertToWorldSpace(cc.v2(0, 0));
     if (!this.battleArea.contains(point)) {
@@ -113,8 +145,10 @@ var Man = cc.Class({
   flyBack: function(dt) {
     let deltaX = MoveDistance * Math.sin(Math.abs(this.hookAngle) * 0.017453293);
     let deltaY = MoveDistance * Math.cos(Math.abs(this.hookAngle) * 0.017453293);
-    this.hook.node.x -= (this.hookAngle > 0 ? -deltaX : deltaX);
-    this.hook.node.y += deltaY;
+    let x = this.hook.node.x - (this.hookAngle > 0 ? -deltaX : deltaX);
+    let y = this.hook.node.y + deltaY;
+    this.setHookPos(x, y);
+    this.ropeMask.node.height -= MoveDistance;
 
     if (this.targetGoldNode !== null) {
       this.targetGoldNode.x -= (this.hookAngle > 0 ? -deltaX : deltaX);
@@ -125,11 +159,13 @@ var Man = cc.Class({
     if (cc.pDistanceSQ(point, this.originPos) < Radius * Radius)
     {
       this.hookState = HookState.Rotation;
-      cc.log(this.targetGoldNode);
-      let gold = this.targetGoldNode.getComponent("Item");
-      this.node.emit("AddScore", { score: gold.score});
-      this.targetGoldNode.destroy();
-      this.targetGoldNode = null;
+      this.ropeMask.node.height = this.maskHeight;
+      if (this.targetGoldNode !== null) {
+        let gold = this.targetGoldNode.getComponent("Item");
+        this.node.emit("AddScore", { score: gold.score });
+        this.targetGoldNode.destroy();
+        this.targetGoldNode = null;
+      }
     }
   },
 })
